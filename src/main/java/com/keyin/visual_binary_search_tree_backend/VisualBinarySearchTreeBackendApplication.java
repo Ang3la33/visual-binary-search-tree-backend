81d6fe194ac53f2a52b342b1751925a73ba66f35
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-04-08 15:57:26 -0230

    Initial commit

---
+++ src/main/java/com/keyin/visual_binary_search_tree_backend/VisualBinarySearchTreeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.visual_binary_search_tree_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualBinarySearchTreeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualBinarySearchTreeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyin/visual_binary_search_tree_backend/VisualBinarySearchTreeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.visual_binary_search_tree_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualBinarySearchTreeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
