94a1dc376a7b9c40dbe4dfabccc557bfd3f1c341
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-04-09 18:55:38 -0230

    Added interface TreeDataRepository that extends JpaRepository taking in TreeData - uses Spring Data JPA for methods such as save and findAll without needing to write SQL

---
+++ src/main/java/com/keyin/visual_binary_search_tree_backend/repository/TreeDataRepository.java
@@ -0,0 +1,6 @@
+package com.keyin.visual_binary_search_tree_backend.repository;
+
+import com.keyin.visual_binary_search_tree_backend.model.TreeData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TreeDataRepository extends JpaRepository<TreeData, Integer> {}
