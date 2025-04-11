ae12e321e6901b5f8749508eda05205b27749c8e
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-04-11 21:21:06 -0230

    Added tests for building BST - returns the correct node, Building empty BST returns null, and building a left dominated tree returns null on right. All tests passed successfully

--- src/test/java/com/keyin/visual_binary_search_tree_backend/VisualBinarySearchTreeBackendApplicationTests.java
+++ src/test/java/com/keyin/visual_binary_search_tree_backend/VisualBinarySearchTreeBackendApplicationTests.java
@@ -1,13 +1,53 @@
 package com.keyin.visual_binary_search_tree_backend;
 
+import com.keyin.visual_binary_search_tree_backend.model.Node;
+import com.keyin.visual_binary_search_tree_backend.service.TreeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 class VisualBinarySearchTreeBackendApplicationTests {
 
+	@Autowired
+	private TreeService treeService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testBuildTree_ReturnsCorrectRoot() {
+		List<Integer> input = List.of(5,3,7);
+		Node root = treeService.buildBinarySearchTree(input);
+
+		assertEquals(5, root.getValue());
+		assertEquals(3, root.getLeft().getValue());
+		assertEquals(7, root.getRight().getValue());
+	}
+
+	@Test
+	void testBuildTreeWithEmptyInput_ReturnsNull() {
+		List<Integer> input = List.of();
+		Node root = treeService.buildBinarySearchTree(input);
+
+		assertNull(root);
+	}
+
+	@Test
+	void testLeftHeavyTreeStructure_ReturnsNullRight() {
+		List<Integer> input = List.of(5,4,3);
+		Node root = treeService.buildBinarySearchTree(input);
+
+		assertEquals(5, root.getValue());
+		assertEquals(4, root.getLeft().getValue());
+		assertEquals(3, root.getLeft().getLeft().getValue());
+		assertNull(root.getRight());
 	}
 
 }
